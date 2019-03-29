3527ebfd4fba007bb64f2061a4b2079597d843a5
Victor Ionescu <dsign.victor@gmail.com>
2019-03-29 18:34:27 +0200

    remove unused css; inline and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,44 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
+const htmlmin = require('gulp-htmlmin');
+const inline = require('gulp-inline');
 
-gulp.task('css', function() {
-  return (
-    gulp
-      .src('./style.css')
-      // ...
-      .pipe(
-        postcss([
-          // ...
-          require('tailwindcss'),
-          require('autoprefixer')
-          // ...
-        ])
-      )
-      // ...
-      .pipe(gulp.dest('build/'))
-  );
+gulp.task('css', () => {
+  return gulp
+    .src('./style.css')
+    .pipe(
+      postcss([
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('uncss').postcssPlugin({
+          html: ['index.html']
+        }),
+        require('cssnano')({
+          preset: 'default'
+        })
+      ])
+    )
+    .pipe(gulp.dest('build/'));
 });
+
+gulp.task(
+  'html',
+  gulp.series('css', () => {
+    return gulp
+      .src('./index.html')
+      .pipe(
+        inline({
+          base: 'build/',
+          disabledTypes: ['svg', 'img', 'js']
+        })
+      )
+      .pipe(
+        htmlmin({
+          collapseWhitespace: true,
+          minifyJS: true,
+          removeComments: true
+        })
+      )
+      .pipe(gulp.dest('docs'));
+  })
+);
