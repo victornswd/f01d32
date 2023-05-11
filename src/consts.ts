68e6c8831efcd7f3409f184059e4763d2a1264ca
Victor Ionescu <dsign.victor@gmail.com>
2023-05-11 19:31:43 +0300

    move to astro + blog templates

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "#f01d32";
+export const SITE_DESCRIPTION = "";
